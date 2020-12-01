72c2ebc78ceafdc842e564ad7f9f64a4275ad2f0
junui <wnsspdl@naver.com>
2020-12-01 15:47:12 +0900

    bug fix

--- app/src/main/java/com/example/omniproject/AuthMainActivity.kt
+++ app/src/main/java/com/example/omniproject/AuthMainActivity.kt
@@ -3,9 +3,9 @@ package com.example.omniproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AuthMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth_main)
     }
 }
